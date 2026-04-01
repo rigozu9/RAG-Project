0d0357910ff96ec7b7da63cef594c22e2560efb0
Riku Nummi <rikunummi9@gmail.com>
2026-04-01 18:58:33 +0300

    Add a check for dtypes and duplicate rows

--- clean_data.py
+++ clean_data.py
@@ -1,4 +1,10 @@
 import pandas as pd
 
 df = pd.read_csv("superstore.csv", encoding="latin1")
-print(df.isnull().sum())
+print(df.isnull().sum()) # 0 in all rows
+
+# Check dtypes
+print(df.dtypes)
+
+# Check duplicate rows
+print("Duplicate rows:", df.duplicated().sum()) # 0
