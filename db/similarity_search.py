e6fc93e44a1d2622827c77b15eaae1e83af57a5b
Riku Nummi <rikunummi9@gmail.com>
2026-04-19 10:21:29 +0300

    Add Ollama answer to similarity search

--- db/similarity_search.py
+++ db/similarity_search.py
@@ -1,5 +1,6 @@
 import chromadb
 from sentence_transformers import SentenceTransformer
+from ollama import chat
 
 CHROMA_PATH = "chroma_db"
 COLLECTION_NAME = "sales_chunks_500"
@@ -28,11 +29,47 @@ def similarity_search(query, top_k):
 
     return matches
 
-query = "technology sales in the east region"
+query = "technology sales in the west region"
 
-for rank, match in enumerate(similarity_search(query, top_k=3), start=1):
-    print(f"Result {rank}")
-    print("ID:", match["id"])
-    print("Distance:", match["distance"])
-    print("Metadata:", match["metadata"])
-    print("Document:", match["document"][:300], "...\n")
+# for rank, match in enumerate(similarity_search(query, top_k=3), start=1):
+#     print(f"Result {rank}")
+#     print("ID:", match["id"])
+#     print("Distance:", match["distance"])
+#     print("Metadata:", match["metadata"])
+#     print("Document:", match["document"][:300], "...\n")
+
+matches = similarity_search(query, top_k=3)
+
+context = ""
+
+for match in matches:
+    context += match["document"] + "\n\n"
+
+prompt = f"""
+Role: You are a sales data analyst. 
+
+Instructions:
+Use only the context below to answer the user's question.
+If the context does not contain enough information, say so.
+
+Context:
+{context}
+
+Based on the context, answer the query.
+Query:
+{query}
+
+Answer:
+"""
+
+response = chat(
+    model="llama3:latest",
+    messages=[
+        {
+            "role": "user",
+            "content": prompt
+        }
+    ]
+)
+
+print(response["message"]["content"])
